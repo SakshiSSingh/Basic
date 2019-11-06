3c6d6d8ac86d3db15228acee3f558d8e2a594381
Sakshi Singh <30416637+SakshiSSingh@users.noreply.github.com>
2019-11-06 08:13:17 +0530

    Update 318A Even and Odds.py

--- 318A Even and Odds.py
+++ 318A Even and Odds.py
@@ -1,6 +1,5 @@
 n, k = map(int, input().split())
-
-if k>int(n/2):
-    print((int(n/2)-k)*2)
+if n%2==0:
+    print( (k-(int(n/2)))*2 if k>(n/2) else(k*2-1) )
 else:
-    print(k*2-1)
+    print( (k-(int(n/2)+1))*2 if k>(n/2+1) else (k*2-1))
