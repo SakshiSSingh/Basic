20f40cc204dd8e9d73a136d8f6fd127ce28a8eca
Sakshi Singh <30416637+SakshiSSingh@users.noreply.github.com>
2019-11-25 15:34:52 +0530

    Update 337A Puzzles.py

--- Codeforces../337A Puzzles.py
+++ Codeforces../337A Puzzles.py
@@ -1,4 +1,8 @@
 n, m = map(int, input().split())
 d = [int(x) for x in input().split()]
+i, l = 0, []
 d=sorted(d)
-print(d[n-1]-d[0])
+while i + n <= len(d):
+	l.append(d[i+n-1] - d[i])
+	i+=1
+print(min(l))
