4ebc14174116d436b9f9d4c466c17abd9e20b7bb
Sakshi Singh <30416637+SakshiSSingh@users.noreply.github.com>
2019-12-11 09:57:00 +0530

    Create 4C Registration System.py

---
+++ Codeforces../4C Registration System.py
@@ -0,0 +1,11 @@
+n = int(input())
+d = {}
+for i in range(0, n):
+    s = str(input())
+    if s in d:
+        print(s+str(d[s]))
+        d[s]+=1
+    else :
+        print('OK')
+        d[s]=1
+
