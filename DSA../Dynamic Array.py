0eb99073c12fb769fcb21e6c8cd6fc3f7e07a2b6
Sakshi Singh <30416637+SakshiSSingh@users.noreply.github.com>
2020-02-05 12:21:48 +0530

    Create Dynamic Array.py

---
+++ DSA../Dynamic Array.py
@@ -0,0 +1,13 @@
+n, q = map(int, input().split())
+lastAns = 0
+seqList = [[] for _ in range(n)]
+for i in range(q):
+    que, x, y = map(int, input().split())
+    seq = (x ^ lastAns)%n
+    if que==1:
+        seqList[seq].append(y)
+    else:
+        lastAns = seqList[seq][y % len(seqList[seq])]
+        print(lastAns)
+    
+Link: https://www.hackerrank.com/challenges/dynamic-array/problem
