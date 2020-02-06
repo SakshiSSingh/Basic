6ca382a6a1f91a5311e63dddd7ad6122d6a5ee04
Sakshi Singh <30416637+SakshiSSingh@users.noreply.github.com>
2020-02-06 15:37:08 +0530

    Update Insert a node at a specific position in a linked list.py

--- HackerRank/Insert a node at a specific position in a linked list.py
+++ HackerRank/Insert a node at a specific position in a linked list.py
@@ -1,32 +1,4 @@
 
-class SinglyLinkedListNode:
-    def __init__(self, node_data):
-        self.data = node_data
-        self.next = None
-
-class SinglyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def insert_node(self, node_data):
-        node = SinglyLinkedListNode(node_data)
-
-        if not self.head:
-            self.head = node
-        else:
-            self.tail.next = node
-
-
-        self.tail = node
-
-def print_singly_linked_list(node, sep):
-    while node:
-        print(str(node.data))
-
-        node = node.next
-
-
 def insertNodeAtPosition(head, data, position):
     counter = 0
     temp = head
@@ -39,19 +11,5 @@ def insertNodeAtPosition(head, data, position):
         counter += 1
     return head
 
-llist_count = int(input())
-
-llist = SinglyLinkedList()
-
-for _ in range(llist_count):
-    llist_item = int(input())
-    llist.insert_node(llist_item)
-
-data = int(input())
-position = int(input())
-
-llist_head = insertNodeAtPosition(llist.head, data, position)
-
-print_singly_linked_list(llist_head, ' ')
 
 Link: https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/problem
